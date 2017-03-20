344763909464ace185e752914ea5d14985afad4c
webdeev <webdevcontato@gmail.com>
2017-03-19 22:30:10 -0300

    site

---
+++ index.php
@@ -0,0 +1,74 @@
+<!doctype html>
+<html lang="pt-br">
+
+
+<head>
+    <meta charset="utf-8">
+
+    <title>WebdeV - Desenvolvilmento Web para todo o Brasil</title>
+
+
+    <meta name="description" content="a WebdeV e uma empresa especializada em desenvolvimento web, ja estamos a 5 anos no mercado em todo o Brasil.">
+
+    <meta name="keywords" content="WebdeV, Desenvolvimento Web, Desenvolvimento de App, Web Design, Design Grafico, Desenvolvimento no Brasil, Linguagem de Programação.">
+
+    <meta name="author" content="WebdeV Desenvolvimento Web">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="icon" href="images/favicon.ico">
+
+    <link rel="stylesheet" href="lebraries/stylesheet.css">
+
+    <script src="lebraries/jquery-3.2.0.js"></script>
+</head>
+
+
+
+
+<body>
+
+<!-- Inicio MENU -->
+<header>
+
+    <nav>
+        <ul class="menu">
+            <li>Home</li>
+            <li>Sobre Nós</li>
+            <li>Anúncios</li>
+            <li>Contato</li>
+            <li>Cadastre-se</li>
+        </ul>
+    </nav>
+
+    <div class="ind-botao">
+        <img src="images/botao.png"> <!-- alt="Botão Mobile" title="Botão Mobile" -->
+    </div>
+
+    <div class="ind-logo">
+        <picture>
+            <source media="(max-width: 480px)" srcset="images/mobile-logo.png">
+            <source media="(min-width: 481px) and (max-width: 768px)" srcset="images/mobile-logo.png">
+            <source media="(min-width: 769px)" srcset="images/desktop-logo.jpg">
+            <img src="images/mobile-logo.png" alt="Logomarca WebdeV Developers" title="Logomarca WebdeV Developers">
+        </picture>
+    </div>
+
+</header>
+<!-- FIM MENU -->
+
+
+<main>
+
+</main>
+
+
+
+<footer>
+
+</footer>
+</body>
+
+
+
+</html>
---
+++ lebraries/javascript.js
@@ -0,0 +1,3 @@
+/**
+ * Created by Jonas on 19/03/2017.
+ */
